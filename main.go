20eec0de365abfda0034f244d4dfae706b31f054
Muhamad Nahrowi <86625224+m-nahrowi@users.noreply.github.com>
2024-03-25 04:35:19 +0700

    add "update"

--- main.go
+++ main.go
@@ -1,5 +1,18 @@
 package main
 
-func main(){
-	
+import (
+	"main.go/database"
+	"main.go/errorhandler"
+	"main.go/router"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load(".env")
+	errorhandler.Check(err)
+	db := database.StartDB(os.Getenv("DB_HOST"), os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_NAME"), os.Getenv("DB_PORT"))
+
+	router.StartService(db).Run(os.Getenv("SERVICE_PORT"))
 }
